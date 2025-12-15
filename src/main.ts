c2199f70c51729402a5f9f432e3323d16c5a88bf
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2025-12-15 12:36:51 +0100

    chore: init commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+// import "./tailwind.css";
+import "./tailwind.scss";
+
+createApp(App).mount("#app");
